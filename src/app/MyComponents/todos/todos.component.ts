55b8409989ef8168887fec9a05bb6d399fc04fca
ninad9 <78981903+ninad9@users.noreply.github.com>
2025-01-27 14:22:05 +0100

    - Corrected Bootstrap css, js and jquer js in angular.json. - Added new component Todo and used it in TodosComponent - Listed down Todo items on view

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent implements OnInit{
+
+  todos!: Todo[];
+constructor(){
+  
+  this.todos = [
+    {
+      sno: 1,
+      title: "Title 1",
+      desc: "Desc 1",
+      active: true
+    },
+    {
+      sno: 2,
+      title: "Title 2",
+      desc: "Desc 2",
+      active: true
+    },
+    {
+      sno: 3,
+      title: "Title 3",
+      desc: "Desc 3",
+      active: false
+    }
+  ]
+}
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+   // throw new Error('Method not implemented.');
   }
 
 }
